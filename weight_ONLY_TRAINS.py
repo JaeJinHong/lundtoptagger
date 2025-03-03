4f3fb9e3903f3552b382f83e57eb4da8c404baa9
Toni Mlinarevic <toni.mlinarevic.20@ucl.ac.uk>
2025-03-03 13:47:41 +0000

    Combined code for training with and without adversarial network

--- weight_ONLY_TRAINS.py
+++ weight_ONLY_TRAINS.py
@@ -55,6 +55,8 @@ def main():
 
     dataset= shuffle(dataset,random_state=42)
     train_ds, validation_ds = train_test_split(dataset, test_size = test_size, random_state = 144)
+    #train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=3)
+    #val_loader = DataLoader(validation_ds, batch_size=batch_size, shuffle=False, num_workers=3)
     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(validation_ds, batch_size=batch_size, shuffle=False)
 
@@ -98,8 +100,15 @@ def main():
     model.to(device)
     
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer_small = torch.optim.Adam(model.parameters(), lr=0.4*learning_rate)
     optimizer2 = torch.optim.Adam(model.parameters(), lr=4*learning_rate)
     optimizer3 = torch.optim.Adam(model.parameters(), lr=10*learning_rate)
+
+    do_combined_training = config['architecture']['do_combined_training']
+    if do_combined_training:
+        adv = Adversary_new(config['architecture']['lambda_parameter'], config['architecture']['num_gaussians'])
+        adv.to(device)
+        optimizer_adv = torch.optim.Adam(adv.parameters(), lr=5*learning_rate)
 
     train_jds = []
     val_jds = []
@@ -128,6 +137,95 @@ def main():
 
     metrics = pd.DataFrame({"Train_Loss":train_loss,"Val_Loss":val_loss})
     metrics.to_csv(metrics_filename, index = False)
+
+    if do_combined_training:
+        adv_model_name = config['architecture']['adv_model_name']
+        loss_parameter = config['architecture']['loss_parameter']
+        loss_weights = config['architecture']['loss_weights']
+        train_loss_clsf = []
+        train_loss_adv = []
+        train_loss_total = []
+        val_loss_clsf = []
+        val_loss_adv = []
+        val_loss_total = []
+        train_acc = []
+        val_acc = []
+
+        MASSBINS = np.linspace(40, 300, (300 - 40) // 5 + 1, endpoint=True)
+        ############ -------------- adversarial pre-trained ------------- ###############
+        for epoch in range(config['architecture']['n_epochs_adv']):
+            ad_lt, clsf_lt, total_lt =  train_adversary_2(train_loader, model, adv, optimizer_adv, device, loss_parameter ,loss_weights) 
+            train_loss_adv.append(ad_lt)
+            train_loss_clsf.append(clsf_lt)
+            train_loss_total.append(total_lt)
+            train_acc.append(get_accuracy(train_loader, model, device))
+    
+            ad_lv, clsf_lv, total_lv =  test_combined(val_loader, model, adv, device, loss_parameter , loss_weights) 
+            val_loss_adv.append(ad_lv)
+            val_loss_clsf.append(clsf_lv)
+            val_loss_total.append(total_lv)
+            val_acc.append(get_accuracy(val_loader, model, device))
+    
+            print('Epoch: {:03d}, Train Loss total: {:.5f}, Train Loss adv: {:.5f}, Train Loss clsf: {:.5f}, val_loss_adv: {:.5f}, val_loss_clsf: {:.5f}, val_loss_total: {:.5f},train_acc: {:.5f},val_acc: {:.5f}'.format(epoch, train_loss_total[epoch],train_loss_adv[epoch],train_loss_clsf[epoch], val_loss_adv[epoch], val_loss_clsf[epoch], val_loss_total[epoch],train_acc[epoch],val_acc[epoch]))
+            metrics = pd.DataFrame({"Train_Loss_adv":train_loss_adv,"Train_Loss_clsf":train_loss_clsf,"Train_Loss_total":train_loss_total,"Val_Loss_Adv":val_loss_adv,"Val_loss_Class":val_loss_clsf,"val_loss_total":val_loss_total, "Train_Acc":train_acc,"Val_Acc":val_acc})
+            metrics.to_csv(metrics_filename, index = False)
+            if (save_every_epoch):
+                torch.save(adv.state_dict(), path_to_save+model_name+adv_model_name+"e{:03d}".format(epoch+1)+"_{:.5f}".format(val_loss_adv[epoch])+".pt")
+            elif epoch == n_epochs-1:
+                torch.save(adv.state_dict(), path_to_save+model_name+adv_model_name+"e{:03d}".format(epoch+1)+"_{:.5f}".format(val_loss_adv[epoch])+".pt")
+
+        train_loss_clsf = []
+        train_loss_adv = []
+        train_loss_total = []
+        val_loss_clsf = []
+        val_loss_adv = []
+        val_loss_total = []
+        train_acc = []
+        val_acc = []
+        train_jsdbg = []
+        val_jsdbg = []
+        for epoch in range(config['architecture']['n_epochs_common']):
+            print("Epoch:{}".format(epoch))
+            if epoch < 12:
+                ad_lt, clsf_lt, total_lt =  train_combined_2(train_loader, model, adv, optimizer_small, optimizer_adv, device, loss_parameter,loss_weights)
+            else:
+                ad_lt, clsf_lt, total_lt =  train_combined_2(train_loader, model, adv, optimizer, optimizer_adv, device, loss_parameter,loss_weights)
+            
+            train_loss_clsf.append(clsf_lt)
+            train_loss_adv.append(ad_lt)
+            train_loss_total.append(total_lt)
+            epsilon_bg, jds = aux_metrics(train_loader, model, adv, device, MASSBINS)
+            #epsilon_bg, jds = 0,0
+            train_jds.append(jds)
+            train_bgrej.append(epsilon_bg)
+            if jds:
+                train_jsdbg.append(epsilon_bg - 1/jds)
+            else:
+                train_jsdbg.append(0)
+
+            ad_lv, clsf_lv, total_lv =  test_combined(val_loader, model, adv, device, loss_parameter, loss_weights)
+            val_loss_adv.append(ad_lv)
+            val_loss_clsf.append(clsf_lv)
+            val_loss_total.append(total_lv)
+    
+            epsilon_bg_test, jds_test = aux_metrics(val_loader, model, adv, device, MASSBINS)
+            #epsilon_bg_test, jds_test = 0,0
+            val_jds.append(jds_test)
+            val_bgrej.append(epsilon_bg_test)
+            if jds_test:
+                val_jsdbg.append(epsilon_bg_test - 1/jds_test)
+            else:
+                val_jsdbg.append(0)
+    
+            print('Epoch: {:03d}, Train Loss total: {:.5f}, Train Loss adv: {:.5f}, Train Loss clsf: {:.5f}, val_loss_adv: {:.5f}, val_loss_clsf: {:.5f}, val_loss_total: {:.5f},train_jds: {:.5f},val_jds: {:.5f},train_jdsbg: {:.5f},val_jdsbg: {:.5f}'.format(epoch,
+                train_loss_total[epoch],train_loss_adv[epoch],train_loss_clsf[epoch], val_loss_adv[epoch], val_loss_clsf[epoch], val_loss_total[epoch], train_jds[epoch], val_jds[epoch],train_jsdbg[epoch],val_jsdbg[epoch]))
+            metrics = pd.DataFrame({"Train_Loss_adv":train_loss_adv,"Train_Loss_clsf":train_loss_clsf,"Train_Loss_total":train_loss_total,"Val_Loss_Adv":val_loss_adv,"Val_loss_Class":val_loss_clsf,
+                "val_loss_total":val_loss_total, "Train_jds":train_jds,"Val_jds":val_jds,"Train_bgrej":train_bgrej,"Val_bgrej":val_bgrej, "Train_jsdbg":train_jsdbg,"Val_jsdbg":val_jsdbg})
+            metrics.to_csv(metrics_filename, index = False)
+            if (save_every_epoch):
+                torch.save(model.state_dict(), path_to_save+model_name+"e{:03d}".format(epoch+1)+"_{:.5f}".format(val_loss_clsf[epoch])+"_comb_"+".pt")
+                torch.save(adv.state_dict(), path_to_save+model_name+adv_model_name+"e{:03d}".format(epoch+1)+"_{:.5f}".format(val_loss_adv[epoch])+"_comb_"+".pt")
+    
     return
 
 
